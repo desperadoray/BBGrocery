1085a6601297b8fb6413239a80b5e76acd0979d1
desperadoray <rza21.sfu@gmail.com>
2017-05-16 01:36:08 -0700

    init vue proj for bbgrocery

---
+++ src/components/Nav.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Nav">
+    NavBar 
+    <div class = ""><span class = "glyphicon glyphicon-barcode"></span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Nav',
+  data () {
+    return {
+      msg: 'Welcome111'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
