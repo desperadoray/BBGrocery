4876159eb09c4be5552cb50c57d921c3bc62ce49
Ray <rza21.ca@gmail.com>
2017-06-02 17:45:49 -0700

    reg

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -1,5 +1,50 @@
 <template>
-	<div class="register">test</div>
+	<div class="album text-muted">
+      <div class="container">
+
+        <div class="row">
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UEBrYkod3YbZfbjOsgPiPkedBc07qJoj0pNz7jChnaAKUlbDgQ" alt="Card image cap">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UEBrYkod3YbZfbjOsgPiPkedBc07qJoj0pNz7jChnaAKUlbDgQ" alt="Card image cap">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UEBrYkod3YbZfbjOsgPiPkedBc07qJoj0pNz7jChnaAKUlbDgQ" alt="Card image cap">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UEBrYkod3YbZfbjOsgPiPkedBc07qJoj0pNz7jChnaAKUlbDgQ" alt="Card image cap">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UEBrYkod3YbZfbjOsgPiPkedBc07qJoj0pNz7jChnaAKUlbDgQ" alt="Card image cap">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UEBrYkod3YbZfbjOsgPiPkedBc07qJoj0pNz7jChnaAKUlbDgQ" alt="Card image cap">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UEBrYkod3YbZfbjOsgPiPkedBc07qJoj0pNz7jChnaAKUlbDgQ" alt="Card image cap">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1UEBrYkod3YbZfbjOsgPiPkedBc07qJoj0pNz7jChnaAKUlbDgQ" alt="Card image cap">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+          <div class="card">
+            <img src="../assets/logo.png" alt="Card image cap">
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
 </template>
 
 <script>
